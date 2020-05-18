8b624e286436b9fc4c65df48674c170eacdce401
chiehjerry <chiehjerry@gmail.com>
2020-05-18 11:17:44 +0800

    fixtheFooter_by_flex
    stillTry-Floatmethod.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,46 +1,42 @@
 <template>
   <footer>
     <div class="container">
-      <div class="row">
-        <div class="col-md-4 col-sm-12">
-          <div class="left">
-            <div class="row">
-              <div class="col-md-6 col-sm-12 left_1">
-                <img src="../assets/footerLogo.png" id="footerLogo" alt />
-              </div>
-              <div class="col-md-3 col-sm-12">
-                <img src="../assets/fb.png" id="fb" alt />
-                <p>粉絲團</p>
-              </div>
-              <div class="col-md-3 col-sm-12">
-                <img src="../assets/line.png" id="line" alt />
-                <p>官方帳號</p>
-              </div>
-              <div class="col-md-12 col-sm-12 copyRight">
-                <p>&copy;2020 一零四資訊科技股份有限公司</p>
-              </div>
+      <div class="footer">
+        <div class="right">
+          <div class="row">
+            <div class="col-md-3 col-sm-12">
+              <p>關於我們</p>
+              <p>常見問題</p>
+            </div>
+            <div class="col-md-3 col-sm-12">
+              <p>服務條款</p>
+              <p>新手教學</p>
+            </div>
+            <div class="col-md-3 col-sm-12">
+              <p>老師合約</p>
+              <p>聯絡我們</p>
+            </div>
+            <div class="col-md-3 col-sm-12">
+              <p>隱私權政策</p>
+              <p>104企業網站</p>
             </div>
           </div>
         </div>
-        <div class="col-md-8 col-sm-12">
-          <div class="right">
-            <div class="row">
-              <div class="col-md-3 col-sm-12">
-                <p>關於我們</p>
-                <p>常見問題</p>
-              </div>
-              <div class="col-md-3 col-sm-12">
-                <p>服務條款</p>
-                <p>新手教學</p>
-              </div>
-              <div class="col-md-3 col-sm-12">
-                <p>老師合約</p>
-                <p>聯絡我們</p>
-              </div>
-              <div class="col-md-3 col-sm-12">
-                <p>隱私權政策</p>
-                <p>104企業網站</p>
-              </div>
+        <div class="left">
+          <div class="row">
+            <div class="col-sm-12 left_1 logo">
+              <img src="../assets/footerLogo.png" id="footerLogo" alt />
+            </div>
+            <div class="col-sm-12 fb">
+              <img src="../assets/fb.png" id="fb" alt />
+              <p>粉絲團</p>
+            </div>
+            <div class="col-sm-12 line_">
+              <img src="../assets/line.png" id="line" alt />
+              <p>官方帳號</p>
+            </div>
+            <div class="col-md-12 col-sm-12 copyRight">
+              <p>&copy;2020 一零四資訊科技股份有限公司</p>
             </div>
           </div>
         </div>
@@ -61,12 +57,8 @@ footer {
   background-color: #ed6c1f;
 }
 
-.left {
-  margin-top: 50px;
-}
-
 .right {
-  margin-top: 100px;
+  padding-top: 100px;
 }
 
 #footerLogo {
@@ -97,9 +89,34 @@ footer .right p {
   font-size: 20px;
 }
 
-@media (max-width: 768px) {
+@media (min-width: 768px) {
+  .footer {
+    display: flex;
+    flex-direction: row-reverse;
+  }
+
+  .left {
+    display: flex;
+    flex: 1;
+  }
+
   .right {
-    margin-top: 10px;
+    flex: 1;
+  }
+  .logo {
+    flex: 1;
+  }
+  .fb {
+    flex: 2;
+  }
+  .line_ {
+    flex: 3;
+  }
+
+  .logo,
+  .fb,
+  .line_ {
+    padding-top: 70px;
   }
 }
 </style>
